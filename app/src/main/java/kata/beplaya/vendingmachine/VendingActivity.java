585c61f19e5e76448574fcfb8cc7c43935e10edd
beplaya <beplaya@gmail.com>
2017-11-08 13:13:34 -0500

    project start

---
+++ app/src/main/java/kata/beplaya/vendingmachine/VendingActivity.java
@@ -0,0 +1,13 @@
+package kata.beplaya.vendingmachine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vending);
+    }
+}
