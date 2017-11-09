c0a7f6da2561bb4ed757b453360d85ce4a3cf499
beplaya <beplaya@gmail.com>
2017-11-09 11:38:53 -0500

    wip return tray

---
+++ app/src/main/java/kata/beplaya/vendingmachine/coin/ReturnTray.java
@@ -0,0 +1,5 @@
+package kata.beplaya.vendingmachine.coin;
+
+public interface ReturnTray {
+    void onReturn(CoinMachine.Coin coin);
+}
--- app/src/test/java/kata/beplaya/vendingmachine/coin/CoinMachineTest.java
+++ app/src/test/java/kata/beplaya/vendingmachine/coin/CoinMachineTest.java
@@ -2,16 +2,25 @@ package kata.beplaya.vendingmachine.coin;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static kata.beplaya.vendingmachine.coin.CoinMachine.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 public class CoinMachineTest {
 
     private CoinMachine coinMachine;
 
+    @Mock
+    private ReturnTray returnTray;
+
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         coinMachine = new CoinMachine();
     }
 
@@ -24,7 +33,7 @@ public class CoinMachineTest {
     public void itCanAcceptValidCoins(){
         assertTrue(coinMachine.insert(Coin.NICKEL));
         assertTrue(coinMachine.insert(Coin.DIME));
-        assertTrue(coinMachine.insert(Coin.QUARTER));s
+        assertTrue(coinMachine.insert(Coin.QUARTER));
     }
 
     @Test
@@ -40,5 +49,19 @@ public class CoinMachineTest {
         coinMachine.insert(Coin.DIME);
         coinMachine.insert(Coin.NICKEL);
         assertEquals(65f, coinMachine.getCurrentAmount(), 0f);
+    }
+
+    @Test
+    public void itHasTheCorrectRejectedCoinsInReturn(){
+        coinMachine.insert(Coin.PENNY);
+        coinMachine.insert(Coin.QUARTER);
+        coinMachine.insert(Coin.QUARTER);
+        coinMachine.insert(Coin.UNRECOGNIZED);
+        coinMachine.insert(Coin.DIME);
+        coinMachine.insert(Coin.NICKEL);
+        coinMachine.insert(Coin.PENNY);
+        verify(returnTray, times(2)).onReturn(Coin.PENNY);
+        verify(returnTray, times(2)).onReturn(Coin.UNRECOGNIZED);
+
     }
 }
